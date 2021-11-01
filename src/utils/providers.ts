b6e5fc9897f0eb0b6dc2eccc3cc78e4f616ab6dd
jaykim <jay@kodax.com>
2021-11-01 15:41:56 +0900

    feat: Use a klaytn node and provider

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -1,8 +1,11 @@
 import { ethers } from 'ethers'
+import Caver from 'caver-js'
 import getRpcUrl from 'utils/getRpcUrl'
 
 const RPC_URL = getRpcUrl()
 
-export const simpleRpcProvider = new ethers.providers.StaticJsonRpcProvider(RPC_URL)
+const caver = new Caver(RPC_URL)
+const klaytnProvider = new caver.providers.HttpProvider(RPC_URL)
+export const simpleRpcProvider = new ethers.providers.Web3Provider(klaytnProvider)
 
 export default null
