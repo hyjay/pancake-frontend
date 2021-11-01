04c4af5df0c1336ae14d5b84549ba7203228c935
jaykim <jay@kodax.com>
2021-11-01 11:28:14 +0900

    feat: Change testnet chain id and a node url with klaytn baobob testnet's

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,22 +1,16 @@
 import sample from 'lodash/sample'
 
-if (
-  process.env.NODE_ENV !== 'production' &&
-  (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.env.REACT_APP_NODE_3)
-) {
-  throw Error('One base RPC URL is undefined')
+if (!process.env.REACT_APP_NODE_PRODUCTION) {
+  throw Error(
+    'Production RPC URL is undeinfed. Check if REACT_APP_NODE_PRODUCTION enviornment variable is defined and valid.',
+  )
 }
 
 // Array of available nodes to connect to
-export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
+export const nodes = [process.env.REACT_APP_NODE_PRODUCTION]
 
 const getNodeUrl = () => {
-  // Use custom node if available (both for development and production)
-  // However on the testnet it wouldn't work, so if on testnet - comment out the REACT_APP_NODE_PRODUCTION from env file
-  if (process.env.REACT_APP_NODE_PRODUCTION) {
-    return process.env.REACT_APP_NODE_PRODUCTION
-  }
-  return sample(nodes)
+  return process.env.REACT_APP_NODE_PRODUCTION
 }
 
 export default getNodeUrl
